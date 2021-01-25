877cfaa54a308e932904f7d9effcb23911d042bb
EC2 Default User <ec2-user@ip-172-31-9-154.us-east-2.compute.internal>
2021-01-25 04:25:08 +0000

    first commit

---
+++ edakh.py
@@ -0,0 +1,9 @@
+"""
+Your module description
+"""
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/iyadsb/edakh.git
+git push -u origin main
